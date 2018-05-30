96d1228916403589b106290f878f8c5b69aabdc5
Paul <pauljohnleonard@gmail.com>
2018-05-30 15:25:45 +0100

    WIP CANDY

--- listsubscribe.ts
+++ listsubscribe.ts
@@ -2,24 +2,29 @@ import { Observable } from 'rxjs/Rx'
 //import { from } from 'rxjs/observable/from'
 
 
-function getsrc(): Array<Observable<string>>{
-    const data = ["A","B","C","D"]
-    
-    const srcs:Array<Observable<string>>  = [];
+function getsrc(): Array<Observable<string>> {
+    const data = ["A", "B", "C", "D"]
+
+    const srcs: Array<Observable<string>> = [];
 
 
     data.forEach((item) => {
-        srcs.push(Observable.of(item));
-    }   );
+        //   srcs.push(Observable.of(item));
+    });
 
     return srcs;
 }
 
-let src=getsrc();
+let src = getsrc();
 
 
 
-Observable.forkJoin(...src).subscribe( (res) => {
+//
+Observable.forkJoin(...src, Observable.create((obs) => {
+    obs.next('XXXX')
+    obs.complete();
+}
+)).subscribe((res) => {
 
     console.log(res);
 
