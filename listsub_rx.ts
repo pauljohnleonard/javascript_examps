97d1a9415eb7b5b1105f284a55adeaa4dcf89601
pauljohnleonard <pauljohnleonard@gmail.com>
2018-05-30 15:24:50 +0100

    WIP

--- listsub_rx.ts
+++ listsub_rx.ts
@@ -21,4 +21,7 @@ const mySyncObs=Observable.bindCallback(myAsync);
 
 let list=[1,2,3,4,5,6]
 
-Observable.from(list).pipe(mergeMap(item => item, 1))
+//Observable.from(list).pipe(mergeMap(item => item, 1))
+
+Observable.from(list).subscribe((item)=>myLog(item))
+
